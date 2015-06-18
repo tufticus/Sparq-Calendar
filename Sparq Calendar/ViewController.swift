1735ea62106b88a3d8ada50463c41d2407017550
Good Shepherd <goodshepherd1@Goods-MacBook-Pro.local>
2015-06-18 18:31:04 -0500

    Initial Commit

---
+++ Sparq Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sparq Calendar
+//
+//  Created by Good Shepherd on 6/18/15.
+//  Copyright (c) 2015 Sparq Calendar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
